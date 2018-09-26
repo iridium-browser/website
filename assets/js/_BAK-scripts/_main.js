60248ca279d311a184445909ac1a8cbcfdceb248
souldanger <ms@souldanger.com>
2018-09-26 16:39:46 +0200

    added link title to #navpanel

--- assets/js/_BAK-scripts/_main.js
+++ assets/js/_BAK-scripts/_main.js
@@ -52,7 +52,7 @@
 			// Title Bar.
 				$(
 					'<div id="titleBar">' +
-						'<a href="#navPanel" class="toggle"></a>' +
+						'<a href="#navPanel" class="toggle" title="menu"></a>' +
 						'<span class="title">' + $('#logo').html() + '</span>' +
 					'</div>'
 				)
