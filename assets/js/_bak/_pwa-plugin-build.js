8e3e2cb536d422f9b3b84493d1b5ee17e543c22a
souldanger <ms@souldanger.com>
2019-04-16 11:14:59 +0200

    revert scripts

--- assets/js/_bak/_pwa-plugin-build.js
+++ assets/js/_bak/_pwa-plugin-build.js
@@ -2,7 +2,6 @@
 window.onload = function () {
     var script = document.createElement('script');
     var firstScript = document.getElementsByTagName('script')[0];
-    script.defer = true;
     script.src = 'XXX?v=' + Date.now();
     firstScript.parentNode.insertBefore(script, firstScript);
 };
