5a2bde39c659aa273d846ec6380102984c27f62c
souldanger <ms@souldanger.com>
2019-04-22 18:58:57 +0200

    minor correction

--- assets/js/_bak/_pwa-plugin-build.js
+++ assets/js/_bak/_pwa-plugin-build.js
@@ -2,6 +2,7 @@
 window.onload = function () {
     var script = document.createElement('script');
     var firstScript = document.getElementsByTagName('script')[0];
+	script.async = true;
     script.src = 'XXX?v=' + Date.now();
     firstScript.parentNode.insertBefore(script, firstScript);
 };
--- assets/js/_bak/_pwa-plugin.js
+++ assets/js/_bak/_pwa-plugin.js
@@ -2,7 +2,6 @@
 window.onload = function () {
     var script = document.createElement('script');
     var firstScript = document.getElementsByTagName('script')[0];
-    script.type = 'text/javascript';
     script.async = true;
     script.src = '{{'sw-register.js'|relative_url}}?v=' + Date.now();
     firstScript.parentNode.insertBefore(script, firstScript);
