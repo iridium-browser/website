590c254feb30b496f1b8bcdc1795d9225d9b2548
souldanger <souldanger@users.noreply.github.com>
2020-05-21 21:22:47 +0200

    minor correction

--- assets/js/_bak/jquery.event-listener.js
+++ assets/js/_bak/jquery.event-listener.js
@@ -1,14 +1,14 @@
 jQuery.event.special.touchstart = 
 {
-  setup: function( _, ns, handle )
-  {
-    if ( ns.includes("noPreventDefault") ) 
-    {
-      this.addEventListener("touchstart", handle, { passive: false });
-    } 
-    else 
-    {
-      this.addEventListener("touchstart", handle, { passive: true });
-    }
-  }
+	setup: function( _, ns, handle )
+	{
+		if ( ns.includes("noPreventDefault") ) 
+		{
+			this.addEventListener("touchstart", handle, { passive: false });
+		} 
+		else 
+		{
+			this.addEventListener("touchstart", handle, { passive: true });
+		}
+	}
 };
